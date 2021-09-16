5ac4a0b6d3385f9f372edb33b24bd12c1cf11836
derricklai <derrickrlai@gmail.com>
2021-09-17 00:54:50 +1000

    fixed bug with sorting functionality, added async error checking

--- src/utilities/format-iso-date-to-AEST.js
+++ src/utilities/format-iso-date-to-AEST.js
@@ -1,8 +1,10 @@
-function convertISODateToAEST(isoDate) {
+function convertISODateToAEST(isoDate, utc = false) {
     try {
         const event =  new Date(isoDate)
         const formatDate = event.toLocaleString('en-AU', { timeZone: 'Australia/Sydney' })
         if(formatDate === 'Invalid Date') throw Error('Please enter a valid date')
+        if (utc)
+            return event
         return `${formatDate} AEST`;
     } catch(error) {
         console.log('Error:', error.message)
